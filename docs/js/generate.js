fc9a1632dd3fca3058d3b35a4681550faa0b776e
Loan Lassalle <18124884+lassalleloan@users.noreply.github.com>
2017-10-11 10:30:43 +0200

    Test to understand

---
+++ docs/js/generate.js
@@ -0,0 +1,31 @@
+$(document).ready(function () {
+    let xhttp = new XMLHttpRequest();
+
+    // Settings
+    const NB_ENTRIES = 10;
+
+    // Makes the request to get the data
+    xhttp.open("GET", "https://raw.githubusercontent.com/lassalleloan/githubAnalytic-static/master/docs/data/data.json");
+    xhttp.responseType = "json";
+    xhttp.send();
+
+    // Callback function when the state is changed
+    xhttp.onreadystatechange = () => {
+
+        // If everything is okay
+        if (xhttp.readyState === XMLHttpRequest.DONE && xhttp.status === 200) {
+
+            let object = xhttp.response;
+
+            // Add dynamically all the repos to the table
+            for (let i = 0; i < object.length; i++) {
+                $("table").append(`<tr><td id="name">${object[i].name}</td><td id="nb_issues">${object[i].nb_issues}</td></tr>`);
+            }
+
+            // Call the drawChart function to draw the chart with de acquired data
+            drawChart(object);
+        }
+    };
+
+
+});
