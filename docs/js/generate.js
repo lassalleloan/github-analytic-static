33440738edd572241070a5e79c01ac0dfdcde813
Loan Lassalle <18124884+lassalleloan@users.noreply.github.com>
2017-10-11 10:39:39 +0200

    Test to understand

--- docs/js/generate.js
+++ docs/js/generate.js
@@ -21,8 +21,10 @@ $(document).ready(function () {
             for (let i = 0; i < object.length; i++) {
                 $("table").append(`<tr><td id="name">${object[i].name}</td><td id="nb_issues">${object[i].nb_issues}</td></tr>`);
             }
+
+            // Call the drawChart function to draw the chart with de acquired data
+            drawChart(object);
         }
     };
-
 
 });
---
+++ docs/js/mychart.js
@@ -0,0 +1,60 @@
+function drawChart(objectList) {
+
+    let ctx = document.getElementById('myChart').getContext('2d');
+
+    // Our custom labels and data
+    mylabels = [];
+    mydata = [];
+
+    // Populate our customs array with our data
+    objectList.forEach((element) => {
+        mylabels.push(element.name);
+        mydata.push(element.nb_issues);
+    });
+
+    let myChart = new Chart(ctx, {
+        type: 'bar',
+        data: {
+            labels: mylabels,
+            datasets: [{
+                label: '# of Issues',
+                data: mydata,
+                backgroundColor: [
+                    'rgba(255, 99, 132, 0.2)',
+                    'rgba(54, 162, 235, 0.2)',
+                    'rgba(255, 206, 86, 0.2)',
+                    'rgba(75, 192, 192, 0.2)',
+                    'rgba(153, 102, 255, 0.2)',
+                    'rgba(255, 159, 64, 0.2)',
+                    'rgba(255, 99, 132, 0.2)',
+                    'rgba(54, 162, 235, 0.2)',
+                    'rgba(255, 206, 86, 0.2)',
+                    'rgba(75, 192, 192, 0.2)'
+                ],
+                borderColor: [
+                    'rgba(255,99,132,1)',
+                    'rgba(54, 162, 235, 1)',
+                    'rgba(255, 206, 86, 1)',
+                    'rgba(75, 192, 192, 1)',
+                    'rgba(153, 102, 255, 1)',
+                    'rgba(255, 159, 64, 1)',
+                    'rgba(255, 99, 132, 1)',
+                    'rgba(54, 162, 235, 1)',
+                    'rgba(255, 206, 86, 1)',
+                    'rgba(75, 192, 192, 1)'
+                ],
+                borderWidth: 1.6
+            }]
+        },
+        options: {
+            responsive: false,
+            scales: {
+                yAxes: [{
+                    ticks: {
+                        beginAtZero: true
+                    }
+                }]
+            }
+        }
+    });
+}
